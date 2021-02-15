01601c40bcbc9582e09a616a5bb5f8b92ba61804
K-LoZ3 <carlos193zambra@gmail.com>
2021-02-15 11:47:39 -0500

    Ejercicio de typescrip. Interfaces

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -86,3 +86,29 @@ function fullNameInic(firstName: string, lastName: string = 'Eduardo'): string {
 }
 
 const carlosCompl = fullNameInic('Carlos'); // Carlos Eduardo
+
+// Interfaces
+interface Rectangulo {
+   ancho: number;
+   alto: number;
+   color?: Color;
+}
+
+let rect: Rectangulo = {
+   ancho: 4,
+   alto: 6,
+   // color: Color.Rojo,
+}
+
+function area(r: Rectangulo): number {
+   return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function() {
+   return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
+}
+
+console.log(rect.toString());
