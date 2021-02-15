ff077bf1dcb447364c1c6433580024878258e448
K-LoZ3 <carlos193zambra@gmail.com>
2021-02-15 10:25:57 -0500

    Ejercicio de serviceWorker. Para que la aplicacion carge archivos en la cache y funcione offline.

--- assets/index.js
+++ assets/index.js
@@ -10,3 +10,9 @@ playButton.onclick = () => player.togglePlay();
 
 const muteButton = document.querySelector('#muteButton');
 muteButton.onclick = () => player.toggleMute();
+
+if('serviceWorker' in navigator) {
+   navigator.serviceWorker.register('../sw.js').catch(err => {
+      console.log(err.message);
+   });
+}
---
+++ sw.js
@@ -0,0 +1,45 @@
+const VERSION = 'v1';
+
+self.addEventListener('install', ev => {
+   ev.waitUntil(precache());
+});
+
+self.addEventListener('fetch', ev => {
+   const request = ev.request;
+   //GET
+   if(request.method /= 'GET') {
+      return;
+   }
+
+   //Buscar en cache
+   ev.respondWith(cachedResponse(request));
+
+   //Actualizar el cache
+   ev.waitUntil(updateCache(request));
+})
+
+async function precache() {
+   const cache = await caches.open(VERSION);
+   cache.addAll([
+      '/',
+      '/index.html',
+      '/assets/index.js',
+      '/assets/mediaPlayer.js',
+      '/assets/plugins/autoPlay.js',
+      '/assets/plugins/autoPause.js',
+      '/assets/index.css',
+      '/assets/BigBuckBunny.mp4',
+   ]);
+}
+
+async function cachedResponse(request) {
+   const cache = await caches.open(VERSION);
+   const response = await cache.match(request);
+   return response || await fetch(request);
+}
+
+async function updateCache(request) {
+   const cache = await caches.open(VERSION);
+   const response = await fetch(request);
+   return cache.put(request, response);
+}
