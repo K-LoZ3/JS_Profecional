3f78b28c50b54e41a335d46d2f7a403a3e72b119
K-LoZ3 <carlos193zambra@gmail.com>
2021-02-17 17:03:10 -0500

    Ejercicio de singleton. Con ayuda del singleton creamos un plugin que muestre un ad cada 30sg.

--- assets/index.ts
+++ assets/index.ts
@@ -1,9 +1,10 @@
 import MediaPlayer from './mediaPlayer';
 import autoPlay from './plugins/autoPlay';
 import autoPause from './plugins/autoPause';
+import Ads from './plugins/ads';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [ new autoPlay(), new autoPause()] });
+const player = new MediaPlayer({ el: video, plugins: [ new autoPlay(), new autoPause(), new Ads()] });
 
 const playButton: HTMLElement = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
